685646d5ead4dec1dc0801866d6b9501820d92ef
BrownSparkles <57475585+BrownSparkles@users.noreply.github.com>
2019-11-07 08:09:14 +0200

    Update data_skew/py
    I have adjusted the code to only look at values in the bias for each columns.

--- data_skew/data_skew.py
+++ data_skew/data_skew.py
@@ -8,8 +8,14 @@ def getBiasedDataset(data: pd.DataFrame, biases: dict) -> pd.DataFrame:
     df = pd.DataFrame()
     for key in biases:
         df_bias = pd.DataFrame.from_dict(biases[key], orient='index', columns=['bias'])
-        df_data = pd.DataFrame(data.groupby(by=f'{key}').size(), columns=['Vol']).reset_index().set_index(key)
+        
+        value_list = []
+        for col in biases[key]:
+            value_list.append(col)
 
+        data_s = data[data[key].isin(value_list)]
+
+        df_data = pd.DataFrame(data_s.groupby(by=f'{key}').size(), columns=['Vol']).reset_index().set_index(key)
         df_processing = df_data.join(df_bias)
         df_processing["Ratio"] = df_processing["Vol"]/df_processing["bias"]
         df_processing["Output"] = df_processing["Ratio"].min() * df_processing["bias"]
@@ -20,8 +26,8 @@ def getBiasedDataset(data: pd.DataFrame, biases: dict) -> pd.DataFrame:
         ds = pd.DataFrame()
 
         for v in dict_n:
-            namespace[f'ds_{count}'] = data[data[key].isin([v])].sample(n=dict_n[v])
+            namespace[f'ds_{count}'] = data_s[data_s[key].isin([v])].sample(n=dict_n[v])
             ds = ds.append(namespace[f'ds_{count}'])
             count += 1
-        data = ds
-    return data
+        data_s = ds
+    return data_s
